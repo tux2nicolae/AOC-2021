fbdbcea06924eee025d1180025b6642c6cb2a78e
Telechi Nicolae <tux2nicolae@gmail.com>
2021-12-09 07:42:35 +0200

    Add solution for day 9

--- C++/Day09/src/main.cpp
+++ C++/Day09/src/main.cpp
@@ -10,6 +10,7 @@
 #include <memory>
 #include <numeric>
 #include <optional>
+#include <queue>
 #include <set>
 #include <sstream>
 #include <string>
@@ -30,11 +31,83 @@ using namespace std;
 int main()
 {
   ifstream in("..\\src\\_input.in");
-  // ofstream out("..\\src\\_output.out");
+  ofstream out("..\\src\\_output.out");
 
   FStreamReader reader(in);
-  auto          input = reader.ReadDataAs<int>();
+  auto          input = reader.ReadDataAsMatrixOfDigits();
 
+  int s = 0;
+
+  vector<AOC::Point> lowPoints;
+
+  for (int x = 0; x < input.size(); x++)
+  {
+    for (int y = 0; y < input[0].size(); y++)
+    {
+      auto point = AOC::Point{ x, y };
+
+      int lowPoint = std::numeric_limits<int>::max();
+      for (auto neighbour : point.GetDirectNeighbours())
+      {
+        if (neighbour.IsInBoundary({ 0, 0 }, { (int)input.size() - 1, (int)input[0].size() - 1 }))
+        {
+          lowPoint = min(lowPoint, input[neighbour.x][neighbour.y]);
+        }
+      }
+
+      if (input[x][y] < lowPoint)
+      {
+        int risklevel = input[x][y] + 1;
+        s += risklevel;
+
+        lowPoints.push_back(AOC::Point{ x, y });
+      }
+    }
+  }
+
+  vector<int> results;
+
+  for (const auto & lowPoint : lowPoints)
+  {
+    queue<AOC::Point> q;
+    q.push(lowPoint);
+
+    map<AOC::Point, bool> visited;
+    int                   basinSize = 0;
+
+    visited[lowPoint] = true;
+
+    while (!q.empty())
+    {
+      auto from = q.front();
+      q.pop();
+
+      basinSize++;
+
+      for (auto to : from.GetDirectNeighbours())
+      {
+        if (!to.IsInBoundary({ 0, 0 }, { (int)input.size() - 1, (int)input[0].size() - 1 }))
+        {
+          continue;
+        }
+
+        if (visited[to] || input[to.x][to.y] == 9)
+        {
+          continue;
+        }
+
+        visited[to] = true;
+        q.push(to);
+      }
+    }
+
+    results.push_back(basinSize);
+  }
+
+  sort(begin(results), end(results), greater<>());
+
+  cout << s << endl;
+  cout << results[0] * results[1] * results[2];
   // out
   // FStreamWriter writer(out);
   // writer.WriteVector(v);
