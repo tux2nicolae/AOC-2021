c48336b1c021a9fcfe324f7ecde797e9f5d0c7fb
Telechi Nicolae <tux2nicolae@gmail.com>
2021-12-11 07:59:24 +0200

    Add solution for day 11

--- C++/Day11/src/main.cpp
+++ C++/Day11/src/main.cpp
@@ -27,13 +27,83 @@ using namespace std;
 #include "../../AOCLib/src/Ship.h"
 #include "../../AOCLib/src/Time.h"
 
+void Flash(vector<vector<int>> & input, map<AOC::Point, bool> & alreadyFlashed, AOC::Point octopus)
+{
+  for (auto & neighbour : octopus.GetAll2DNeighbours())
+  {
+    if (!neighbour.IsInBoundary({ 0, 0 }, { (int)input.size() - 1, (int)input.size() - 1 }))
+      continue;
+
+    if (alreadyFlashed[neighbour])
+      continue;
+
+    if (++input[neighbour.x][neighbour.y] > 9)
+    {
+      input[neighbour.x][neighbour.y] = 0;
+
+      alreadyFlashed[neighbour] = true;
+      Flash(input, alreadyFlashed, neighbour);
+    }
+  }
+}
+
 int main()
 {
   ifstream in("..\\src\\_input.in");
-  // ofstream out("..\\src\\_output.out");
+  ofstream out("..\\src\\_output.out");
 
   FStreamReader reader(in);
-  auto          input = reader.ReadDataAs<int>();
+  auto          input = reader.ReadDataAsMatrixOfDigits();
+
+  int steps = 0;
+  while (++steps)
+  {
+    int totalFlashes = 0;
+
+    vector<AOC::Point>    shouldFlashes;
+    map<AOC::Point, bool> alreadyFlashed;
+
+    for (int x = 0; x < input.size(); x++)
+    {
+      for (int y = 0; y < input[0].size(); y++)
+      {
+        AOC::Point octopus{ x, y };
+        if (++input[octopus.x][octopus.y] > 9)
+        {
+          input[octopus.x][octopus.y] = 0;
+
+          alreadyFlashed[octopus] = true;
+          shouldFlashes.push_back(octopus);
+        }
+      }
+    }
+
+    for (auto & shouldFlash : shouldFlashes)
+    {
+      Flash(input, alreadyFlashed, shouldFlash);
+    }
+
+    FStreamWriter writer(out);
+    writer.WriteMatrix(input);
+
+    out << endl;
+
+    for (int x = 0; x < input.size(); x++)
+    {
+      for (int y = 0; y < input[0].size(); y++)
+      {
+        if (input[x][y] == 0)
+          totalFlashes++;
+      }
+    }
+
+    if (totalFlashes == 100)
+    {
+      break;
+    }
+  }
+
+  cout << steps;
 
   // out
   // FStreamWriter writer(out);
