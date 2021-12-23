6415715236cd2dce0cb47c5866bd3686b223f32d
Telechi Nicolae <tux2nicolae@gmail.com>
2021-12-23 21:31:48 +0200

    Refactor code to make it more readable

--- C++/Day23/src/main.cpp
+++ C++/Day23/src/main.cpp
@@ -29,231 +29,227 @@ using namespace std;
 #include "../../AOCLib/src/Ship.h"
 #include "../../AOCLib/src/Time.h"
 
-auto GetEnergy(char amphipod)
-{
-  switch (amphipod)
-  {
-  case 'A':
-    return 1;
-  case 'B':
-    return 10;
-  case 'C':
-    return 100;
-  case 'D':
-    return 1000;
-  }
-
-  assert(false);
-  return 0;
-}
-
 struct Amphenoid
 {
+  auto GetEnergy() const
+  {
+    switch (id)
+    {
+    case 'A':
+      return 1;
+    case 'B':
+      return 10;
+    case 'C':
+      return 100;
+    case 'D':
+      return 1000;
+    }
+
+    assert(false);
+    return 0;
+  }
+
   char       id;
   AOC::Point position;
 };
 
 struct Room
 {
-  char              roomTarget;
+  bool HasImpostors()
+  {
+    for (const auto & roomMate : roomMates)
+    {
+      if (roomMate.id != roomTarget)
+        return true;
+    }
+
+    return false;
+  }
+
+  char              roomTarget{};
   vector<Amphenoid> roomMates;
-
-  optional<int> roomStartingRow{};
-  int           roomCol{};
-  int           maxSize{};
+  AOC::Point        roomPosition{};
 };
 
-bool CanGoToHallway(const map<AOC::Point, char> & hallways,
-                    AOC::Point                    hallway,
-                    Amphenoid &                   amphipod)
+struct Space
 {
-  assert(hallways.count(hallway));
+  Space() { rooms.resize(4); }
 
-  if (hallways.at(hallway) != '.')
-    return false;
-
-  auto nextHallway = hallway;
-  int  stepY       = 0;
-
-  if (amphipod.position.y < hallway.y)
+  bool IsOrganized()
   {
-    nextHallway.y = amphipod.position.y + 1;
-    stepY         = 1;
+    for (const auto & room : rooms)
+    {
+      if (room.roomMates.size() != roomSize)
+        return false;
+
+      for (const auto & roomMate : room.roomMates)
+      {
+        if (roomMate.id != room.roomTarget)
+          return false;
+      }
+    }
+
+    return true;
   }
-  else
+
+  Room & GetTargetRoomFor(const Amphenoid & amphipod)
   {
-    nextHallway.y = amphipod.position.y - 1;
-    stepY         = -1;
+    assert(amphipod.id - 'A' < rooms.size());
+    return rooms[amphipod.id - 'A'];
+  };
+
+  vector<AOC::Point> GetAvailableHallwaysFor(const Amphenoid & amphipod)
+  {
+    vector<AOC::Point> availableHallways;
+    for (const auto & [hallway, _] : hallways)
+      if (CanGoToHallway(hallway, amphipod))
+        availableHallways.push_back(hallway);
+
+    return availableHallways;
   }
 
-  for (; nextHallway != hallway; nextHallway.y += stepY)
+  bool CanGoToHallway(const AOC::Point & hallway, const Amphenoid & amphipod)
   {
-    if (!hallways.count(nextHallway))
-      continue;
-
-    if (hallways.at(nextHallway) != '.')
+    assert(hallways.count(hallway));
+    if (hallways.at(hallway) != '.')
       return false;
+
+    auto nextHallway = hallway;
+    int  stepY       = 0;
+
+    if (amphipod.position.y < hallway.y)
+    {
+      nextHallway.y = amphipod.position.y + 1;
+      stepY         = 1;
+    }
+    else
+    {
+      nextHallway.y = amphipod.position.y - 1;
+      stepY         = -1;
+    }
+
+    for (; nextHallway != hallway; nextHallway.y += stepY)
+    {
+      if (!hallways.count(nextHallway))
+        continue;
+
+      if (hallways.at(nextHallway) != '.')
+        return false;
+    }
+
+    return true;
   }
 
-  return true;
-}
-
-bool CanGoToTargetRoom(map<AOC::Point, char> & hallways, AOC::Point hallway, Room & room)
-{
-  if (hallways[hallway] != room.roomTarget)
-    return false;
-
-  assert(room.roomMates.size() <= 4);
-
-  for (auto roomMate : room.roomMates)
+  bool CanGoToTargetRoom(AOC::Point hallway, Room & room)
   {
-    if (roomMate.id != room.roomTarget)
+    assert(hallways.count(hallway));
+    if (hallways.at(hallway) != room.roomTarget)
       return false;
+
+    assert(room.roomMates.size() <= 4);
+    for (const auto & roomMate : room.roomMates)
+    {
+      if (roomMate.id != room.roomTarget)
+        return false;
+    }
+
+    AOC::Point targetRoomPoint{ room.roomPosition.x + (roomSize - room.roomMates.size() - 1),
+                                room.roomPosition.y };
+
+    auto nextHallway = hallway;
+    int  stepY       = 0;
+
+    if (targetRoomPoint.y < hallway.y)
+    {
+      nextHallway.y = hallway.y - 1;
+      stepY         = -1;
+    }
+    else if (targetRoomPoint.y > hallway.y)
+    {
+      nextHallway.y = hallway.y + 1;
+      stepY         = 1;
+    }
+
+    for (; nextHallway.y != targetRoomPoint.y; nextHallway.y += stepY)
+    {
+      if (!hallways.count(nextHallway))
+        continue;
+
+      if (hallways.at(nextHallway) != '.')
+        return false;
+    }
+
+    return true;
   }
 
-  AOC::Point targetRoomPoint{ *room.roomStartingRow + (room.maxSize - room.roomMates.size() - 1),
-                              room.roomCol };
-
-  auto nextHallway = hallway;
-  int  stepY       = 0;
-
-  if (targetRoomPoint.y < hallway.y)
+  int CostToHallway(const AOC::Point & hallway, const Amphenoid & amphipod)
   {
-    nextHallway.y = hallway.y - 1;
-    stepY         = -1;
+    return AOC::ManhattenDistance(amphipod.position, hallway) * amphipod.GetEnergy();
   }
-  else if (targetRoomPoint.y > hallway.y)
+
+  int CostToTargetRoom(const Room & toRoom, const AOC::Point & fromHallway, Amphenoid & amphipod)
   {
-    nextHallway.y = hallway.y + 1;
-    stepY         = 1;
+    assert(toRoom.roomMates.size() <= 3);
+    AOC::Point targetRoomPoint{ toRoom.roomPosition.x + (roomSize - toRoom.roomMates.size() - 1),
+                                toRoom.roomPosition.y };
+
+    return AOC::ManhattenDistance(targetRoomPoint, fromHallway) * amphipod.GetEnergy();
   }
 
-  for (; nextHallway.y != targetRoomPoint.y; nextHallway.y += stepY)
-  {
-    if (!hallways.count(nextHallway))
-      continue;
+  //-------------------------------------------------------------------------
 
-    if (hallways[nextHallway] != '.')
-      return false;
-  }
+  map<AOC::Point, char> hallways;
 
-  return true;
-}
-
-int GetTargetRoomFor(const Amphenoid & amphipod)
-{
-  return amphipod.id - 'A';
+  size_t       roomSize{};
+  vector<Room> rooms;
 };
 
-vector<AOC::Point> GetAvailableHallwaysFor(const map<AOC::Point, char> & hallways,
-                                           Amphenoid &                   amphipod)
-{
-  vector<AOC::Point> availableHallways;
-  for (auto [hallway, _] : hallways)
-    if (CanGoToHallway(hallways, hallway, amphipod))
-      availableHallways.push_back(hallway);
-
-  return availableHallways;
-}
-
-int GoToHallway(map<AOC::Point, char> & hallways, AOC::Point hallway, Amphenoid & amphipod)
-{
-  hallways[hallway] = amphipod.id;
-  return AOC::ManhattenDistance(amphipod.position, hallway) * GetEnergy(amphipod.id);
-}
-
-int GoToTargetRoom(map<AOC::Point, char> & hallways,
-                   Room &                  room,
-                   AOC::Point              hallway,
-                   Amphenoid &             amphipod)
-{
-  assert(room.roomMates.size() <= 3);
-
-  AOC::Point targetRoomPoint{ *room.roomStartingRow + (room.maxSize - room.roomMates.size() - 1),
-                              room.roomCol };
-
-  return AOC::ManhattenDistance(targetRoomPoint, hallway) * GetEnergy(amphipod.id);
-}
-
-bool AreOrganized(const vector<Room> & rooms)
-{
-  bool found = true;
-  for (auto & room : rooms)
-  {
-    if (room.roomMates.size() != room.maxSize)
-    {
-      found = false;
-    }
-
-    bool allOff = true;
-    for (auto & roomMate : room.roomMates)
-    {
-      allOff = allOff && roomMate.id == room.roomTarget;
-    }
-
-    found = found && allOff;
-  }
-
-  return found;
-}
-
-bool HasImpostors(const Room & room)
-{
-  for (const auto & roomMate : room.roomMates)
-  {
-    if (roomMate.id != room.roomTarget)
-      return true;
-  }
-
-  return false;
-}
-
-optional<int> Solve(ofstream & out, map<AOC::Point, char> hallways, vector<Room> rooms)
+optional<int> Solve(Space space)
 {
   int initialCost = 0;
 
   // move from hallway to rooms
-  int n = 5;
+  int n = space.hallways.size();
   while (n--)
   {
-    for (const auto & [hallway, amphenoidId] : hallways)
+    for (const auto & [hallway, amphenoidId] : space.hallways)
     {
       if (amphenoidId == '.')
         continue;
 
       auto   amphenoid  = Amphenoid{ amphenoidId, hallway };
-      auto & targetRoom = rooms[GetTargetRoomFor(amphenoid)];
+      auto & targetRoom = space.GetTargetRoomFor(amphenoid);
 
-      if (CanGoToTargetRoom(hallways, hallway, targetRoom))
+      if (space.CanGoToTargetRoom(hallway, targetRoom))
       {
-        initialCost += GoToTargetRoom(hallways, targetRoom, hallway, amphenoid);
+        initialCost += space.CostToTargetRoom(targetRoom, hallway, amphenoid);
 
-        hallways[hallway] = '.';
+        space.hallways[hallway] = '.';
         targetRoom.roomMates.push_back(amphenoid);
       }
     }
   }
 
-  if (AreOrganized(rooms))
+  if (space.IsOrganized())
     return initialCost;
 
-  //-------------------------------------------------------------------------
-
   optional<int> minCost;
-  for (auto & room : rooms)
+  for (auto & room : space.rooms)
   {
-    if (!HasImpostors(room))
+    if (!room.HasImpostors())
       continue;
 
     Amphenoid amphenoid = room.roomMates.back();
-    for (const auto & hallway : GetAvailableHallwaysFor(hallways, amphenoid))
+    for (const auto & hallway : space.GetAvailableHallwaysFor(amphenoid))
     {
       // move to hallways
-      int cost = GoToHallway(hallways, hallway, amphenoid);
+      space.hallways[hallway] = amphenoid.id;
       room.roomMates.pop_back();
 
+      int cost = space.CostToHallway(hallway, amphenoid);
+
       // solve the subproblem
-      auto subproblemCost = Solve(out, hallways, rooms);
+      auto subproblemCost = Solve(space);
       if (subproblemCost)
       {
         minCost =
@@ -261,7 +257,7 @@ optional<int> Solve(ofstream & out, map<AOC::Point, char> hallways, vector<Room>
       }
 
       // revert back
-      hallways[hallway] = '.';
+      space.hallways[hallway] = '.';
       room.roomMates.push_back(amphenoid);
     }
   }
@@ -277,53 +273,42 @@ int main()
   FStreamReader reader(in);
   auto          lines = reader.ReadLines();
 
-  map<AOC::Point, char> hallways;
+  Space space;
 
-  vector<Room> rooms;
-  rooms.resize(4);
-
-  vector<char> targets{ 'A', 'B', 'C', 'D' };
-
-  for (int i = lines.size() - 1; i >= 0; i--)
+  // parse space data
+  const vector<char> kTargets{ 'A', 'B', 'C', 'D' };
+  for (int x = lines.size() - 1; x >= 0; x--)
   {
-    int roomId = 0;
-
-    for (int j = 0; j < lines[i].size(); j++)
+    for (int roomNumber = 0, y = 0; y < lines[x].size(); y++)
     {
-      AOC::Point point{ i, j };
-      if (lines[i][j] == '.' && lines[i + 1][j] == '#')
+      char       id = lines[x][y];
+      AOC::Point point{ x, y };
+
+      if (id == '.' && lines[x + 1][y] == '#')
       {
-        hallways[point] = '.';
+        space.hallways[point] = '.';
       }
-      else if (lines[i][j] >= 'A' && lines[i][j] <= 'D')
+      else if (id >= 'A' && id <= 'D')
       {
-        auto roomMate = Amphenoid{ lines[i][j], point };
+        Amphenoid roomMate{ id, point };
+        space.rooms[roomNumber].roomMates.push_back(roomMate);
+        space.rooms[roomNumber].roomTarget = kTargets[roomNumber];
 
-        rooms[roomId].roomTarget = targets[roomId];
-        rooms[roomId].roomCol    = j;
+        space.roomSize = max(space.roomSize, space.rooms[roomNumber].roomMates.size());
 
-        rooms[roomId].roomMates.push_back(roomMate);
-        rooms[roomId++].maxSize =
-          max((size_t)rooms[roomId].maxSize, rooms[roomId].roomMates.size());
+        roomNumber++;
       }
     }
   }
 
-  // set room starting row
-  for (int i = 0; i < lines.size(); i++)
+  // parse room starting position
+  for (int roomNumber = 0, y = 0; y < lines[2].size(); y++)
   {
-    int roomId = 0;
-    for (int j = 0; j < lines[i].size(); j++)
-    {
-      if (lines[i][j] >= 'A' && lines[i][j] <= 'D')
-      {
-        if (!rooms[roomId].roomStartingRow)
-          rooms[roomId++].roomStartingRow = i;
-      }
-    }
+    if (lines[2][y] >= 'A' && lines[2][y] <= 'D')
+      space.rooms[roomNumber++].roomPosition = { 2, y };
   }
 
-  auto totalCost = Solve(out, hallways, rooms);
+  auto totalCost = Solve(space);
   cout << *totalCost;
 
   return 0;
